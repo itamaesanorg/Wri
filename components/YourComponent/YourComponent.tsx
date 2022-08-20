770703fdf26b5d791a32c99a4ba29b4e857e97d7
miguelgargallo <mgargallo16@gmail.com>
2022-08-21 00:26:02 +0200

    a

--- components/YourComponent/YourComponent.tsx
+++ components/YourComponent/YourComponent.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable prettier/prettier */
 export default function YourComponent() {
   return (
     <div className='hidden xl:inline'>
       <div>
-      <a>Place your ✒️ Component here</a>
+        <a>Place your ✒️ Component here</a>
       </div>
     </div>
   )
