d18dcd6b97dc79739544558fe063b30b9679ab1d
miguelgargallo <mgargallo16@gmail.com>
2022-08-20 22:33:07 +0200

    0.0.3

--- components/YourComponent/YourComponent.tsx
+++ components/YourComponent/YourComponent.tsx
@@ -8,8 +8,8 @@ export default function Sushiten() {
     <div className='hidden xl:inline'>
       <motion.div
         className='px-8 py-4 drop-shadow-xl max-w-6xl w-full bg-white-800 z-[1] filter-blur dark:bg-gray-800 top-10 md:top-20 rounded-md mx-auto flex justify-between items-center'
-        const animate={{ y: -10 }}
-        const transition={{ duration: 0.5 }}
+        animate={{ y: -10 }}
+        transition={{ duration: 0.5 }}
       >
       
       <a>Place your ✒️ Component here</a>
