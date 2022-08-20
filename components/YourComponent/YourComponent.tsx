62131c18eaa069db34a15751437808dc25cce739
miguelgargallo <mgargallo16@gmail.com>
2022-08-21 00:16:55 +0200

    1.0.0 Framer disabled

--- components/YourComponent/YourComponent.tsx
+++ components/YourComponent/YourComponent.tsx
@@ -1,20 +1,16 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
 
-import { motion } from 'framer-motion'
+import motion from '../../node_modules/framer-motion'
 
 export default function Sushiten() {
   return (
     <div className='hidden xl:inline'>
-      <motion.div
-        className='px-8 py-4 drop-shadow-xl max-w-6xl w-full bg-white-800 z-[1] filter-blur dark:bg-gray-800 top-10 md:top-20 rounded-md mx-auto flex justify-between items-center'
-        animate={{ y: -10 }}
-        transition={{ duration: 0.5 }}
-      >
+      <div>
       
       <a>Place your ✒️ Component here</a>
       
-      </motion.div>
+      </div>
     </div>
   )
 }
